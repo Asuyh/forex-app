df5a3c6f98777b2c498bc78d06b3dd3b5e5ee38f
Ayush KC <sidecar_aloof0o@icloud.com>
2026-01-05 17:53:10 +0545

    added tailwind css $ frontend started

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ForexController;
+Route::get('/forex',[ForexController::class, 'index'])
+->middleware('throttle:30,1');
+
+Route::get('/', function () {
+    return view('forex.index');
+})->name('homeindex');
